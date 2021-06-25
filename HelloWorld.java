3b3405a97c92a5e59c3b52f9c396725f535f71bf
EC2 Default User <ec2-user@ip-172-31-40-25.ap-south-1.compute.internal>
2021-06-25 10:43:56 +0000

    commit 1

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,19 +2,8 @@ public class HelloWorld
 {   
       public static void main(String[] args)
       {       
-         System.out.println("Hello guys");
+         System.out.println("Hello world");
       	 
-         System.out.println("Hello guys");
-         System.out.println("Hello guys");
-         System.out.println("Hello guys");
-         System.out.println("Hello guys");
-         System.out.println("Hello guys");
-         System.out.println("Hello guys");
-         System.out.println("Hello guys");
-         System.out.println("Hello guys");
-         System.out.println("Hello guys");
-         System.out.println("Hello guys");
-         System.out.println("Hello guys");
       	 
       	 
       	 
---
+++ Hellobabe.java
@@ -0,0 +1,12 @@
+public class Hellobabe
+{
+      public static void main(String[] args)
+      {
+         System.out.println("Hello babe");
+
+
+
+
+      }
+}
+
